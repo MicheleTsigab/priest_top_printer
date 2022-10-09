c8e68b3d2f14941fddaadf107d50339e2b43690e
Michele <micheletsigab@gmail.com>
2022-10-09 16:48:04 +0300

    filter for check and prints

--- generator/admin.py
+++ generator/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Printer,Check
+class PrinterAdmin(admin.ModelAdmin):
+    list_filter = ("check_type",)
 
-# Register your models here.
+
+class CheckAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Check._meta.fields]
+    list_filter = ("printer_id", "type", "status")
+
+
+admin.site.register(Printer, PrinterAdmin)
+admin.site.register(Check, CheckAdmin)
